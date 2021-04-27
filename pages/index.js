d5c5ac1804f26cf73f4921ed116f872557098142
Chonwai <chonwaiun@gmail.com>
2021-04-27 13:14:13 +0800

    Delete: Deleted some useless codes and files.

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,12 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { useEffect } from 'react';
-import { TextStyle } from '@shopify/polaris';
+import { Page, TextStyle } from '@shopify/polaris';
 
 export default function Home() {
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
-                </h1>
-                <p className="font-bold">HiHi</p>
-                <p className={styles.description}>
-                    Get started by editing{' '}
-                    <code className={styles.code}>pages/index.js</code>
-                </p>
-                <TextStyle variation="positive">
-                    Sample app using React and Next.js
-                </TextStyle>
-            </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <img
-                        src="/vercel.svg"
-                        alt="Vercel Logo"
-                        className={styles.logo}
-                    />
-                </a>
-            </footer>
-        </div>
+        <Page>
+            <TextStyle variation="positive">
+                Sample app using React and Next.js
+            </TextStyle>
+        </Page>
     );
 }
